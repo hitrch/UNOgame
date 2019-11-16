cf8d489bd4f368522e2a4b3cf70a4a4b073892b5
hitrch <vladislawpaliy@gmail.com>
2019-11-16 16:01:16 +0200

    adding frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+    render() {
+    return (
+        <div className="Checkin">
+            <h1>
+                Hello
+            </h1>
+        </div>
+    );
+  }
+}
+
+export default App;
