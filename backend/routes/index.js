245991f12a47479c57aeec924dd7cbf9a8206eea
hitrch <vladislawpaliy@gmail.com>
2019-11-16 23:44:17 +0200

    continuing to fix proxy problems№10

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("../../frontend/public/index.html");
+  res.sendFile("../../frontend/public/index.html");
 });
 
 
