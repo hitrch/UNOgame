26f88c8781472939bcee490ada2359efd8f2749b
hitrch <vladislawpaliy@gmail.com>
2019-11-17 18:14:49 +0200

    adding tg widget#1

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/api/getList', (req,res) => {
-  let list = ["item1", "item2", "item3"];
-  res.json(list);
+router.get('/', (req,res) => {
+
   console.log('Sent list of items');
 });
 
