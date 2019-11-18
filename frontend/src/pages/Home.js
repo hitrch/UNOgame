177361502ab9e36dede85f350bc071a743fe67b9
hitrch <vladislawpaliy@gmail.com>
2019-11-18 11:54:05 +0200

    adding tg widget#16

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -20,15 +20,13 @@ class Home extends Component {
     render() {
         return (
             <div className="App">
-                <Link to={'./list'}>
-                    <h6>
-                        Welcome to UNOgame
-                    </h6>
 
                 <script async src="https://telegram.org/js/telegram-widget.js?7" data-telegram-login="UNOgBot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
-                </Link>
                 <div ref={el => (this.instance = el)} />;
-            }
+
+                <h6>
+                    Welcome to UNOgame
+                </h6>
             </div>
         );
     }
