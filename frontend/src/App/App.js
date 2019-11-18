bcab4a7ce83f3b177dc8653a4bade93beda2d4bc
hitrch <vladislawpaliy@gmail.com>
2019-11-18 15:41:08 +0200

    adding tg widget#36

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -8,12 +8,17 @@ import Login from '../pages/Login';
 
 
 class App extends Component {
+
+    componentDidMount() {
+        const log = document.createElement("div");
+        log.id = 'login';
+        log.innerHTML = "<script async src='https://telegram.org/js/telegram-widget.js?7' data-telegram-login='UNOgBot' data-size='large' data-onauth='onTelegramAuth(user)' data-request-access='write'></script>";
+        document.appendChild(log);
+    }
+
     render() {
         const App = () => (
             <div>
-                <div id = 'login'>
-                    <script async src="https://telegram.org/js/telegram-widget.js?7" data-telegram-login="UNOgBot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
-                </div>
                 <Switch>
                     <Route exact path='/' component={Login}/>
                     <Route path='/list' component={List}/>
