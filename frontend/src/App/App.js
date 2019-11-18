cb792e5b1c680912c0d8557a432893c8c97de7bb
hitrch <vladislawpaliy@gmail.com>
2019-11-18 17:20:09 +0200

    adding tg widget#43

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from '../pages/Home';
 import List from '../pages/List';
-import Login from '../pages/Login';
 
 
 import {render} from "react-dom";
@@ -22,14 +21,6 @@ render(
 
 
 class App extends Component {
-
-    componentDidMount() {
-        const log = document.createElement("div");
-        log.id = 'login';
-        log.innerHTML = "<script async src='https://telegram.org/js/telegram-widget.js?7' data-telegram-login='UNOgBot' data-size='large' data-onauth='onTelegramAuth(user)' data-request-access='write'></script>";
-        document.body.prepend(log);
-    }
-
     render() {
         const App = () => (
             <div>
