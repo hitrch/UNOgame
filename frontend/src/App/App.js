a3e81013f772cb5127d595c4299c4c217fe30a4b
hitrch <vladislawpaliy@gmail.com>
2019-11-18 15:22:42 +0200

    adding tg widget#33

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -18,9 +18,9 @@ class App extends Component {
             </div>
         );
         return (
-            <Switch>
-                <App/>
-            </Switch>
+            <div id = 'login'>
+                <script async src="https://telegram.org/js/telegram-widget.js?7" data-telegram-login="UNOgBot" data-size="large" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+            </div>
         );
     }
 }
