4c4e5efe678fdbe1859d5deb82763372c9cdf6eb
hitrch <vladislawpaliy@gmail.com>
2019-11-19 18:50:50 +0200

    adding tg widget#53

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App/App';
-import TelegramLoginButton from 'react-telegram-login';
-
-
 
 render((
     <BrowserRouter>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,13 +8,22 @@ class Home extends Component {
         super(props);
     }
 
-    onTelegramAuth(user) {
-        console.log(user);
+    componentDidMount() {
+        const s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.innerHTML = "function onTelegramAuth(user) {\n" +
+            "    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');\n" +
+            "  }";
+        document.body.appendChild(s);
     }
 
     render() {
         return (
             <div className="App">
+                <div id = "login">
+                    <script async src="https://telegram.org/js/telegram-widget.js?7" data-telegram-login="UNOgBot" data-size="small" data-onauth="onTelegramAuth(user)" data-request-access="write"></script>
+                </div>
                     <h6>
                         Welcome to UNOgame
                     </h6>
