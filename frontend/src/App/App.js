5fdcd9ea3155cbf4e4cdb2b9c803f116a4c21934
hitrch <vladislawpaliy@gmail.com>
2019-11-19 17:35:35 +0200

    adding tg widget#47

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -4,29 +4,23 @@ import './App.css';
 import Home from '../pages/Home';
 import List from '../pages/List';
 
-
-import {render} from "react-dom";
-import TelegramLoginButton from "react-telegram-login";
-
-
-const handleTelegramResponse = response => {
-    console.log(response);
-};
-
-function onTelegramAuth(user) {
-    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-}
-
-render(
-    <TelegramLoginButton dataOnauth={handleTelegramResponse} botName="UNOgBot"/>,
-    document.getElementById('telegramButton')
-);
-
-
-
 class App extends Component {
+    componentDidMount() {
+        const s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.innerHTML = "Telegram.Passport.createAuthButton('telegram_passport_auth', {\n" +
+            "            bot_id:       123456, // place id of your bot here\n" +
+            "            scope:        {data: [{type: 'id_document', selfie: true}, 'address_document', 'phone_number', 'email'], v: 1},\n" +
+            "            public_key:   '-----BEGIN PUBLIC KEY----- ...', // place public key of your bot here\n" +
+            "            nonce:        'ab2df83746a87d2f3bd6...', // place nonce here\n" +
+            "            callback_url: 'https://unog.herokuapp.com/callback/' // place callback url here\n" +
+            "        });";
+        this.instance.appendChild(s);
+    }
+
     render() {
         const App = () => (
+
             <div>
                 <Switch>
                     <Route exact path='/' component={Home}/>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-
-
 class Home extends Component {
 
     render() {
         return (
             <div className="App">
-                <Link to={Home}>
                     <h6>
                         Welcome to UNOgame
                     </h6>
-                </Link>
             </div>
         );
     }
