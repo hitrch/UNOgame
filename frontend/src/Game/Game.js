03b99c1b15205863474a48994d0c1722dcbe8e6b
hitrch <vladislawpaliy@gmail.com>
2019-12-07 15:37:42 +0200

    adding header#3

--- frontend/src/Game/Game.js
+++ frontend/src/Game/Game.js
@@ -36,7 +36,7 @@ class Game extends Component {
 
 function mapStateToProps(state) {
     return{
-        userInfo: loginSelectors.getUser(state).firstName
+        userInfo: loginSelectors.getUser(state).first_name
     };
 }
 
